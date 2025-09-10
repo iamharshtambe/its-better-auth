ae76898dfb853f8853c142086f47f5b9a6802ee2
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-10 21:07:43 +0530

    feat: setup db with prisma

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,6 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from './generated/prisma';
-import { nextCookies } from 'better-auth/next-js';
 
 const prisma = new PrismaClient();
 
@@ -10,5 +9,4 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  plugins: [nextCookies()],
 });
