392061f9416ce491664a041f3e70c633659f7b03
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-10 22:48:14 +0530

    implement email password auth

--- app/auth/auth-client.tsx
+++ app/auth/auth-client.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+// import { useRouter, useSearchParams } from 'next/navigation';
 import { signIn, signUp } from '@/lib/actions/auth-actions';
 
 export default function AuthClientPage() {
@@ -11,8 +11,8 @@ export default function AuthClientPage() {
   const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
-  const router = useRouter();
-  const searchParams = useSearchParams();
+  // const router = useRouter();
+  // const searchParams = useSearchParams();
 
   // Get callback URL from search params (set by middleware)
 
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,13 @@
-import AuthClientPage from "./auth-client";
+import { auth } from '@/lib/auth';
+import AuthClientPage from './auth-client';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default async function AuthPage() {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (session) {
+    redirect('/dashboard');
+  }
   return <AuthClientPage />;
 }
