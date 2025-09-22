b82ee07559f3b5709344353c7625a48c9ce215fb
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-22 21:06:41 +0530

    add mount handler

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,20 @@
 import { Hono } from 'hono';
 import { getTodos } from './db/queries';
+import { auth } from './lib/auth';
 
 const app = new Hono();
 
-const router = app.get('/api/todos', async (c) => {
-  try {
-    const todos = await getTodos();
-    return c.json(todos);
-  } catch (error) {
-    console.log('Failed to fetch todos', error);
-    return c.json({ error: 'Failed to fetch todos' }, 500);
-  }
-});
+const router = app
+  .on(['POST', 'GET'], '/api/auth/*', (c) => auth.handler(c.req.raw))
+  .get('/api/todos', async (c) => {
+    try {
+      const todos = await getTodos();
+      return c.json(todos);
+    } catch (error) {
+      console.log('Failed to fetch todos', error);
+      return c.json({ error: 'Failed to fetch todos' }, 500);
+    }
+  });
 
 export type AppType = typeof router;
 export default app;
