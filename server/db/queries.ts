44a904dc49297cd5e086423e24a71362cc9a095a
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-22 14:00:05 +0530

    add get todos query

---
+++ server/db/queries.ts
@@ -0,0 +1,7 @@
+import { desc } from 'drizzle-orm';
+import { db } from '.';
+import { todosTable } from './schema';
+
+export async function getTodos() {
+  return await db.select().from(todosTable).orderBy(desc(todosTable.createdAt));
+}
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,16 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { getTodos } from './db/queries';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/api/todos', async (c) => {
+  try {
+    const todos = await getTodos();
+    return c.json(todos);
+  } catch (error) {
+    console.log('Failed to fetch todos', error);
+    return c.json({ error: 'Failed to fetch todos' }, 500);
+  }
+});
 
-export default app
+export default app;
