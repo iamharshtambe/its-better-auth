1f89d11874cc2949974c35d978e71e8ba058499a
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-23 17:47:08 +0530

    add sign up form

--- client/src/routes/signup.tsx
+++ client/src/routes/signup.tsx
@@ -6,8 +6,54 @@ export const Route = createFileRoute('/signup')({
 
 function RouteComponent() {
   return (
-    <div>
-      <button className="btn">Button</button>
+    <div className="flex justify-center items-center min-h-screen bg-base-100">
+      <form className="w-full max-w-sm space-y-4 p-6 rounded-2xl shadow-md bg-base-100">
+        <h1 className="text-2xl font-bold text-center">Sign Up</h1>
+
+        {/* Name */}
+        <div>
+          <label className="input validator w-full">
+            <input type="text" placeholder="Full Name" required minLength={2} />
+          </label>
+          <div className="validator-hint hidden">Enter your name</div>
+        </div>
+
+        {/* Email */}
+        <div>
+          <label className="input validator w-full">
+            <input type="email" placeholder="mail@site.com" required />
+          </label>
+          <div className="validator-hint hidden">Enter valid email address</div>
+        </div>
+
+        {/* Password */}
+        <div>
+          <label className="input validator w-full">
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              minLength={6}
+            />
+          </label>
+          <div className="validator-hint hidden">
+            Password must be at least 6 characters
+          </div>
+        </div>
+
+        {/* Confirm Password */}
+        <div>
+          <label className="input validator w-full">
+            <input type="password" placeholder="Confirm Password" required />
+          </label>
+          <div className="validator-hint hidden">Passwords must match</div>
+        </div>
+
+        {/* Submit */}
+        <button className="btn btn-neutral w-full" type="submit">
+          Create Account
+        </button>
+      </form>
     </div>
   )
 }
