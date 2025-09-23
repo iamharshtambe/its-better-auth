d586643cd40187232576011d8c4aee61020a1b7d
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-23 21:03:30 +0530

    add active state to nav-links

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,14 +5,14 @@ export default function Header() {
     <nav className="flex items-center justify-between px-4 py-2">
       <h1 className="text-3xl font-bold">Todos</h1>
 
-      <ul className="flex items-center justify-between gap-4 text-lg font-semibold text-neutral-500">
-        <Link to="/">
+      <ul className="flex items-center justify-between gap-4 text-lg text-neutral-500">
+        <Link to="/" activeProps={{ className: 'font-bold' }}>
           <li className="hover:border px-1 hover:rounded-2xl hover:bg-neutral-200">
             Home
           </li>
         </Link>
 
-        <Link to="/todos">
+        <Link to="/todos" activeProps={{ className: 'font-bold' }}>
           <li className="hover:border px-1 hover:rounded-2xl hover:bg-neutral-200">
             Todos
           </li>
