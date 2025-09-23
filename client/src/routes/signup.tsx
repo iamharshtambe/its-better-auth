758a577dab97434f8c4e57bae62f3c97f7025275
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-23 18:15:51 +0530

    add v1 signup

--- client/src/routes/signup.tsx
+++ client/src/routes/signup.tsx
@@ -1,19 +1,66 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
+import { createFileRoute, useRouter } from '@tanstack/react-router'
+import { authClient } from '@/lib/auth-client'
 
 export const Route = createFileRoute('/signup')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const router = useRouter()
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    setError('')
+
+    if (password !== confirmPassword) {
+      setError('Password does not match')
+      return
+    }
+
+    try {
+      authClient.signUp.email({
+        name,
+        email,
+        password,
+      })
+
+      router.navigate({
+        to: '/todos',
+      })
+    } catch (err) {
+      setError('An unexpected error occur')
+      console.log('Error', err)
+    }
+  }
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-base-100">
-      <form className="w-full max-w-sm space-y-4 p-6 rounded-2xl shadow-md bg-base-100">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-sm space-y-4 p-6 rounded-2xl shadow-md bg-base-100"
+      >
         <h1 className="text-2xl font-bold text-center">Sign Up</h1>
+
+        {error && <div>{error}</div>}
 
         {/* Name */}
         <div>
           <label className="input validator w-full">
-            <input type="text" placeholder="Full Name" required minLength={2} />
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Full Name"
+              required
+              minLength={2}
+            />
           </label>
           <div className="validator-hint hidden">Enter your name</div>
         </div>
@@ -21,7 +68,13 @@ function RouteComponent() {
         {/* Email */}
         <div>
           <label className="input validator w-full">
-            <input type="email" placeholder="mail@site.com" required />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="mail@site.com"
+              required
+            />
           </label>
           <div className="validator-hint hidden">Enter valid email address</div>
         </div>
@@ -31,6 +84,8 @@ function RouteComponent() {
           <label className="input validator w-full">
             <input
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
               required
               minLength={6}
@@ -44,7 +99,13 @@ function RouteComponent() {
         {/* Confirm Password */}
         <div>
           <label className="input validator w-full">
-            <input type="password" placeholder="Confirm Password" required />
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder="Confirm Password"
+              required
+            />
           </label>
           <div className="validator-hint hidden">Passwords must match</div>
         </div>
