fd02b6fd128964d2c8b67ecd767ed3b8fc846bc2
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-24 21:56:01 +0530

    add auth middleware

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,12 @@
 import { Hono } from 'hono';
-import { getTodos } from './db/queries';
 import { auth } from './lib/auth';
+import { todos } from './routes/todos.route';
 
 const app = new Hono();
 
 const router = app
   .on(['POST', 'GET'], '/api/auth/*', (c) => auth.handler(c.req.raw))
-  .get('/api/todos', async (c) => {
-    try {
-      const todos = await getTodos();
-      return c.json(todos);
-    } catch (error) {
-      console.log('Failed to fetch todos', error);
-      return c.json({ error: 'Failed to fetch todos' }, 500);
-    }
-  });
+  .route('/todos', todos);
 
 export type AppType = typeof router;
 export default app;
